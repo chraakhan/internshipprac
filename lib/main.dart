f524acb7ababc0b4c5b6a8fae5c7fc2910776219
chraakhan <chrafaraj3@gmail.com>
2026-06-06 14:07:36 +0300

    localization implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
@@ -16,13 +19,68 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("🔥 Firebase Connected Successfullyyyyyyyyyyyyyyy"),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final nameController = TextEditingController();
+  final ageController = TextEditingController();
+
+  Future<void> sendData() async {
+    await FirebaseFirestore.instance.collection('students').add({
+      'name': nameController.text,
+      'age': ageController.text,
+      'time': DateTime.now().toString(),
+    });
+
+    nameController.clear();
+    ageController.clear();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text("Data sent to Firebase 🔥")),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Firebase Form")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                labelText: "Enter your name",
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: ageController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: "Enter your age",
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: sendData,
+              child: const Text("Send to Firebase"),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-//helloooooooooo wolrld 22222
